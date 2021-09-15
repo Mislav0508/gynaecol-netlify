8153360df1dcfa170a60b556694f93f4772a5713
Mislav0508 <mislav0508@hotmail.com>
2021-09-15 14:48:00 +0200

    cors

--- server/Config/db.js
+++ server/Config/db.js
@@ -3,10 +3,17 @@ require('dotenv').config()
 
 // MYSQL
 var db = mysql.createConnection({
-  host: 'eu-cdbr-west-01.cleardb.com',
-  user: 'bc6a1c4990f14f',
-  password: '8570391c',
-  database: 'heroku_cf47678c333c77d'
+  host: 'localhost',
+  user: 'root',
+  password: 'Mislav05080639',
+  database: 'gynaecol'
 })
+
+// var db = mysql.createConnection({
+//   host: 'eu-cdbr-west-01.cleardb.com',
+//   user: 'bc6a1c4990f14f',
+//   password: '8570391c',
+//   database: 'heroku_cf47678c333c77d'
+// })
 
 module.exports = db
--- server/app.js
+++ server/app.js
@@ -6,7 +6,13 @@ var cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) 
 app.use(routes)
